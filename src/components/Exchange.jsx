d09d115fe2862850eda9afb17ae3bd3f3367d5a1
unknown <vsagar267@gmai;.com>
2023-02-11 13:26:10 +0530

    css improvement in exchange

--- src/components/Exchange.jsx
+++ src/components/Exchange.jsx
@@ -35,7 +35,7 @@ const Exchange = () => {
               <div className={`flex flex-row gap-4`}>
                 <li key={exchange.id} className={`list-none sm:px-12 py-4 bg-[#A66EFF] w-[160px] sm:w-[300px] mt-4 text-center text-[16px] text-white font-normal `}>{exchange.title}</li>
                 <div key={2*exchange.id} className={`list-none px-4 py-4 bg-[#A66EFF] w-[100px] mt-4 text-center text-[16px] text-white font-normal`}>{exchange.rate}</div>
-                <img src={exchange.img} alt={exchange.site} key={exchange.site} className={`w-[50px] text-white mt-4 hover:bg-[#A66EFF] hover:cursor-pointer hover:rounded-full`} onClick={()=>window.open(exchange.site)}/>
+                <img src={exchange.img} alt={exchange.site} key={exchange.site} className={`w-[50px] text-white mt-6 hover:bg-[#A66EFF] hover:cursor-pointer hover:rounded-full`} onClick={()=>window.open(exchange.site)}/>
               </div>
             </section>
           ))}
