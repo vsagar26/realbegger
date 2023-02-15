67dd885c23b68e8a33fda6fbca8b2afed2181a10
unknown <vsagar267@gmai;.com>
2023-02-15 20:21:35 +0530

    bg change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Navbar, Hero, Feature, Exchange, Payment, Footer} from "./components";
 
 function App() {
   return (
-    <div className="App bg-[#353535] ">
+    <div className="App bg-[#ffffff] "> 
       <Navbar/>
       <Hero/>
       <Feature/>
